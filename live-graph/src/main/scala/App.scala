8c6db291c5e57369030f00c8fe61aabb58541d83
Patrik Pettersson <patrik.x.pettersson@scania.com>
2022-09-08 10:25:47 +0200

    Graph State Monad

--- live-graph/src/main/scala/App.scala
+++ live-graph/src/main/scala/App.scala
@@ -5,12 +5,12 @@ import cats.effect.syntax.all.{*, given}
 
 import scala.concurrent.ExecutionContext.global
 import cats.effect.{ExitCode, IO, IOApp, Ref}
-import se.randomserver.geometry.Graph
+import se.randomserver.geometry.{Graph, GraphState}
 import se.randomserver.linear.V2
 import linear.syntax.{*, given}
 import geometry.syntax.{*, given}
 
-import cats.data.OptionT
+import cats.data.{OptionT, State}
 import cats.effect.IO.{IOCont, Uncancelable}
 import fs2.{Pipe, Stream}
 
@@ -18,81 +18,86 @@ import java.util.UUID
 
 object App extends IOApp:
   type ID = UUID
-  def lastPipe(lastRef: Ref[IO, Map[ID, Graph.NodeId]]) : Pipe[IO, (ID, Point[V2, Double]), (ID, Option[Graph.NodeId], Point[V2, Double])] = stream =>
-    stream.evalMap {
-      case (id, value) => for
-        last <- lastRef.get.map(_.get(id))
-      yield (id, last, value)
-    }
+  //Idef lastPipe(lastRef: Ref[IO, Map[ID, Graph.NodeId]]) : Pipe[IO, (ID, Point[V2, Double]), (ID, Option[Graph.NodeId], Point[V2, Double])] = stream =>
+  //I  stream.evalMap {
+  //I    case (id, value) => for
+  //I      last <- lastRef.get.map(_.get(id))
+  //I    yield (id, last, value)
+  //I  }
 
-  def insertPipe(grapRef: Ref[IO, Graph[V2, Double]]): Pipe[IO, (ID, Option[Graph.NodeId], Point[V2, Double]), (ID, Graph.NodeId)] = stream =>
-    stream.evalMapFilter {
-      case (id, lastIdO, nextPoint) => for // Vehicle has been some where before
-        graph <- grapRef.get
-        dLastPoint    = lastIdO.map(lastId => lastId -> qdA(graph.vertices(lastId), nextPoint))
-        dClosestEdge  = graph.closestEdge(nextPoint)
-        dClosestPoint = graph.closestVertex(nextPoint)
+  //Idef insertPipe(grapRef: Ref[IO, Graph[V2, Double]]): Pipe[IO, (ID, Option[Graph.NodeId], Point[V2, Double]), (ID, Graph.NodeId)] = stream =>
+  //I  stream.evalMapFilter {
+  //I    case (id, lastIdO, nextPoint) => for // Vehicle has been some where before
+  //I      graph <- grapRef.get
+  //I      dLastPoint    = lastIdO.map(lastId => lastId -> qdA(graph.vertices(lastId), nextPoint))
+  //I      dClosestEdge  = graph.closestEdge(nextPoint)
+  //I      dClosestPoint = graph.closestVertex(nextPoint)
 
-        newPointId <- (dLastPoint, dClosestEdge, dClosestPoint) match
-          case (Some((lastId, dl)), Some((edgeId, de)), Some((pointId, dp))) => // we come from a point and there are both points and edges present (common case)
-            if de < dp && de < dl && de < 1.0 then // closest to a edge within 1 m
-              grapRef.modify { graph => graph.insert(nextPoint, edgeId).get }
-            else if dl < de && dl < 1.0 then // closer to last point than any other point within 1 m
-              val updated = lerp(0.4, graph.vertices(lastId), nextPoint)
-              grapRef.modify { graph => graph.update(lastId, updated) }
-            else if dp < dl && dp < 1.0 then // closer to some other point within 1 m
-              val updated = lerp(0.4, graph.vertices(pointId), nextPoint)
-              grapRef.modify { graph => graph.update(pointId, updated) }
-            else // more than 1 m from any edge or point create a new edge to lastPoint
-              grapRef.modify { graph =>
-                val (ng, np) = graph.addPoint(nextPoint)
-                val (nng, _) = ng.addEdge(lastId, np)
-                nng -> np
-              }
-          case (Some((lastId, dl)), None, Some((pointId, dp))) => // We come from apoint and there are no edges in graph
-            if dl < dp && dl < 1.0 then // within 1 m from last point
-              val updated = lerp(0.4, graph.vertices(lastId), nextPoint)
-              grapRef.modify { graph => graph.update(lastId, updated) }
-            else if dp < 1.0 then
-              val updated = lerp(0.4, graph.vertices(pointId), nextPoint)
-              grapRef.modify { graph => graph.update(pointId, updated) }
-            else
-              grapRef.modify { graph =>
-                val (ng, np) = graph.addPoint(nextPoint)
-                val (nng, _) = ng.addEdge(lastId, np)
-                nng -> np
-              }
-          case (None, Some((edgeId, de)), Some((pointId, dp))) =>
-            if de < dp && de < 1.0 then
-              grapRef.modify { graph => graph.insert(nextPoint, edgeId).get }
-            else if dp < 1.0 then
-              val updated = lerp(0.4, graph.vertices(pointId), nextPoint)
-              grapRef.modify { graph => graph.update(pointId, updated) }
-            else // Weird case just add a point
-              grapRef.modify { graph => graph.addPoint(nextPoint) }
-          case (None, None, None) =>
-            grapRef.modify { graph => graph.addPoint(nextPoint) }
-      yield Some(id -> newPointId)
-    }
+  //I      newPointId <- (dLastPoint, dClosestEdge, dClosestPoint) match
+  //I        case (Some((lastId, dl)), Some((edgeId, de)), Some((pointId, dp))) => // we come from a point and there are both points and edges present (common case)
+  //I          if de < dp && de < dl && de < 1.0 then // closest to a edge within 1 m
+  //I            grapRef.modify { graph => graph.insert(nextPoint, edgeId).get }
+  //I          else if dl < de && dl < 1.0 then // closer to last point than any other point within 1 m
+  //I            val updated = lerp(0.4, graph.vertices(lastId), nextPoint)
+  //I            grapRef.modify { graph => graph.update(lastId, updated) }
+  //I          else if dp < dl && dp < 1.0 then // closer to some other point within 1 m
+  //I            val updated = lerp(0.4, graph.vertices(pointId), nextPoint)
+  //I            grapRef.modify { graph => graph.update(pointId, updated) }
+  //I          else // more than 1 m from any edge or point create a new edge to lastPoint
+  //I            grapRef.modify { graph =>
+  //I              val (ng, np) = graph.addPoint(nextPoint)
+  //I              val (nng, _) = ng.addEdge(lastId, np)
+  //I              nng -> np
+  //I            }
+  //I        case (Some((lastId, dl)), None, Some((pointId, dp))) => // We come from apoint and there are no edges in graph
+  //I          if dl < dp && dl < 1.0 then // within 1 m from last point
+  //I            val updated = lerp(0.4, graph.vertices(lastId), nextPoint)
+  //I            grapRef.modify { graph => graph.update(lastId, updated) }
+  //I          else if dp < 1.0 then
+  //I            val updated = lerp(0.4, graph.vertices(pointId), nextPoint)
+  //I            grapRef.modify { graph => graph.update(pointId, updated) }
+  //I          else
+  //I            grapRef.modify { graph =>
+  //I              val (ng, np) = graph.addPoint(nextPoint)
+  //I              val (nng, _) = ng.addEdge(lastId, np)
+  //I              nng -> np
+  //I            }
+  //I        case (None, Some((edgeId, de)), Some((pointId, dp))) =>
+  //I          if de < dp && de < 1.0 then
+  //I            grapRef.modify { graph => graph.insert(nextPoint, edgeId).get }
+  //I          else if dp < 1.0 then
+  //I            val updated = lerp(0.4, graph.vertices(pointId), nextPoint)
+  //I            grapRef.modify { graph => graph.update(pointId, updated) }
+  //I          else // Weird case just add a point
+  //I            grapRef.modify { graph => graph.addPoint(nextPoint) }
+  //I        case (None, None, None) =>
+  //I          grapRef.modify { graph => graph.addPoint(nextPoint) }
+  //I    yield Some(id -> newPointId)
+  //I  }
 
-  def updateLast(lastRef: Ref[IO, Map[ID, Graph.NodeId]]): Pipe[IO, (ID, Graph.NodeId), Unit] = stream => {
-    stream.evalMap {
-      case (id, id1) => lastRef.update(last => last.updated(id, id1))
-    }
-  }
+  //Idef updateLast(lastRef: Ref[IO, Map[ID, Graph.NodeId]]): Pipe[IO, (ID, Graph.NodeId), Unit] = stream => {
+  //I  stream.evalMap {
+  //I    case (id, id1) => lastRef.update(last => last.updated(id, id1))
+  //I  }
+  //I}
 
   override def run(args: List[String]): IO[ExitCode] = for
-    last  <- Ref.of[IO, Map[ID, Graph.NodeId]](Map.empty)
-    graph <- Ref.of[IO, Graph[V2, Double]](Graph[V2, Double]())
-    id = UUID.randomUUID()
-    stream = Stream(V2(0.0, 0.0), V2(0.6, 0.1), V2(1.5, 0.0), V2(2.0, 2.0))
-      .map(id -> Point(_))
-      .through(lastPipe(last))
-      .through(insertPipe(graph))
-      .through(updateLast(last))
-    _ <- stream.compile.drain
-    graph <- graph.get
-    _ = println(graph.segments)
+    //last  <- Ref.of[IO, Map[ID, Graph.NodeId]](Map.empty)
+    //graph <- Ref.of[IO, Graph[V2, Double]](Graph[V2, Double]())
+    //id = UUID.randomUUID()
+    //stream = Stream(V2(0.0, 0.0), V2(0.6, 0.1), V2(1.5, 0.0), V2(2.0, 2.0))
+    //  .map(id -> Point(_))
+    //  .through(lastPipe(last))
+    //  .through(insertPipe(graph))
+    //  .through(updateLast(last))
+    //_ <- stream.compile.drain
+    //graph <- graph.get
+    //_ = println(graph.segments)
+    graphRef <- Ref.of[IO, Graph[V2, Long]](Graph[V2, Long])
+
+
+    v <- graphRef.modifyState(Graph.vertices)
+
   yield ExitCode.Success
 
 end App
--- src/main/scala/geometry/Graph.scala
+++ src/main/scala/geometry/Graph.scala
@@ -3,29 +3,20 @@ package geometry
 
 import linear.{Additive, LinearIntegral, Metric}
 
-import cats.{Apply, Foldable}
+import cats.{Applicative, Apply, Eval, FlatMap, Foldable}
 import linear.Affine.Point
 import geometry.LineSegment
 import linear.syntax.{*, given}
 import geometry.syntax.{*, given}
 
-object Graph:
-  import java.util.UUID
-
-  opaque type NodeId = UUID
-
-  object NodeId:
-    def apply(): NodeId = UUID.randomUUID()
-
-  opaque type EdgeId = UUID
-
-  object EdgeId:
-    def apply(): EdgeId = UUID.randomUUID()
-
-  case class Edge(from: NodeId, to: NodeId)
-end Graph
+import cats.data.{IndexedStateT, State, StateT}
+import cats.syntax.applicative.{*, given}
 
 import Graph.*
+
+type GraphStateT[F[_], P[_], A, R] = StateT[F, Graph[P, A], R]
+type GraphState[P[_], A, R] = GraphStateT[Eval, P, A, R]
+
 
 case class Graph[P[_]: Foldable: Apply: Metric: Additive, A: LinearIntegral](vertices: Map[NodeId,Point[P, A]] = Map.empty[NodeId, Point[P, A]], edges: Map[EdgeId, Edge] = Map.empty[EdgeId, Edge]):
   def closestEdge(p: Point[P, A]): Option[(EdgeId, A)] = edges.map {
@@ -67,3 +58,49 @@ case class Graph[P[_]: Foldable: Apply: Metric: Additive, A: LinearIntegral](ver
   def segments: Set[LineSegment[P, A]] = edges.map {
     case _ -> Edge(from, to) => LineSegment(vertices(from), vertices(to))
   }.toSet
+end Graph
+
+object Graph:
+  import java.util.UUID
+
+  opaque type NodeId = UUID
+
+  object NodeId:
+    def apply(): NodeId = UUID.randomUUID()
+
+  opaque type EdgeId = UUID
+
+  object EdgeId:
+    def apply(): EdgeId = UUID.randomUUID()
+
+  case class Edge(from: NodeId, to: NodeId)
+
+
+  def apply[P[_]: Foldable: Apply: Metric: Additive, A: LinearIntegral]: Graph[P, A] = Graph()
+
+  def run[P[_], A, R](initial: Graph[P, A])(f: GraphState[P, A, R]): (Graph[P, A], R) = f.run(initial).value
+  def runF[F[_]: Applicative: FlatMap, P[_], A, R](initial: Graph[P, A])(f: GraphStateT[F, P, A, R]): F[(Graph[P, A], R)] = f.run(initial)
+
+  def liftF[F[_]: Applicative, P[_], A, R](fr: F[R]): IndexedStateT[F, Graph[P, A], Graph[P, A], R] = StateT.liftF[F, Graph[P, A], R](fr)
+
+  def addPoint[F[_]: Applicative, P[_], A](p: Point[P, A]): GraphStateT[F, P, A, NodeId] = StateT(graph => graph.addPoint(p).pure[F])
+  def addEdge[F[_]: Applicative, P[_], A](n1: NodeId, n2: NodeId): GraphStateT[F, P, A, EdgeId] = StateT(graph => graph.addEdge(n1, n2).pure[F])
+
+  def insert[F[_]: Applicative, P[_], A](p: Point[P ,A], e: EdgeId): GraphStateT[F, P, A, Option[NodeId]] =
+    StateT(graph => graph.insert(p, e).map(i => i._1 -> Some(i._2)).getOrElse(graph -> None).pure[F])
+
+  def update[F[_]: Applicative, P[_], A](n: NodeId, p: Point[P, A]): GraphStateT[F, P, A, NodeId] = StateT(graph => graph.update(n, p).pure[F])
+
+  def closestEdge[F[_]: Applicative, P[_], A](p: Point[P, A]): GraphStateT[F, P, A, Option[(EdgeId, A)]] = StateT(graph => (graph -> graph.closestEdge(p)).pure[F])
+
+  def closestVertex[F[_]: Applicative,P[_], A](p: Point[P, A]): GraphStateT[F, P, A, Option[(NodeId, A)]] = StateT(graph => (graph -> graph.closestVertex(p)).pure[F])
+
+  def edges[F[_]: Applicative, P[_], A]: GraphStateT[F, P, A, Map[EdgeId, Edge]]  = StateT.get.map(s => s.edges)
+
+  def vertices[F[_]: Applicative, P[_], A]: GraphStateT[F, P, A, Map[NodeId, Point[P, A]]] = StateT.get.map(s => s.vertices)
+
+
+
+
+
+end Graph
